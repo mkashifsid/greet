bf5e6a1a84c0b2f99a937057e7f0d0ebfd343f46
Kashif <kashif.siddiqui@bykea.com>
2021-11-15 11:57:39 +0500

    sentinel connection code added in the code

--- greet.py
+++ greet.py
@@ -1,5 +1,6 @@
 import json
 import redis
+from redis.sentinel import Sentinel
 from flask import Flask, request
 from typing import Text, Optional, Dict, Any
 
@@ -9,8 +10,11 @@ app = Flask(__name__)
 def get_current_user() -> Optional[Dict[Text, Any]]:
     """Extract current user details from storage."""
 
-    red = redis.StrictRedis(host="localhost", port=6379, db=1)
-    encoded_user = red.get("user")
+    """red = redis.StrictRedis(host="redis.default.svc.cluster.local", port=6379, db=1)"""
+    sentinel = Sentinel([('sentinel.default.svc.cluster.local',5000)], socket_timeout=0.1)
+    master = sentinel.master_for('mymaster', socket_timeout=0.1)
+    slave = sentinel.slave_for('mymaster', socket_timeout=0.1)
+    encoded_user = slave.get("user")
     if encoded_user:
         return json.loads(encoded_user)
     else:
@@ -20,8 +24,10 @@ def get_current_user() -> Optional[Dict[Text, Any]]:
 def store_user(user: Dict[Text, Any]) -> None:
     """Save user details to our storage."""
 
-    red = redis.StrictRedis(host="localhost", port=6379, db=1)
-    red.set("user", json.dumps(user))
+    """red = redis.StrictRedis(host="redis.default.svc.cluster.local", port=6379, db=1)"""
+    sentinel = Sentinel([('sentinel.default.svc.cluster.local',5000)], socket_timeout=0.1)
+    master = sentinel.master_for('mymaster', socket_timeout=0.1)
+    master.set("user", json.dumps(user))
 
 
 @app.route('/', methods=["GET"])
